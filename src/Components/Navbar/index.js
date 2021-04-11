5b773e7625ce32906bbf33f2c318cd753d7d6c1c
JCPosso <JCPosso@users.noreply.github.com>
2021-04-11 12:52:08 -0500

    navbar ok

---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {FaBars} from 'react-icons/fa';
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    MobileIcon,
+    NavMenu,
+    NavItems,
+    NavLinks,
+    NavBtn,
+    NavBtnLink
+} from './NavbarElements';
+const Navbar = () => {
+    return (
+        <>
+        {/*create with rafce command'*/}
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to='/'>JCPosso</NavLogo>
+                    <MobileIcon>
+                        <FaBars/>
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItems>
+                            <NavLinks to="about">Home</NavLinks>
+                        </NavItems>
+                        <NavItems>
+                            <NavLinks to="education">Education</NavLinks>
+                        </NavItems>
+                        <NavItems>
+                            <NavLinks to="projects">Projects</NavLinks>
+                        </NavItems>
+                        <NavItems>
+                            <NavLinks to="contact">Contact</NavLinks>
+                        </NavItems>
+                    </NavMenu>
+
+                </NavbarContainer>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
