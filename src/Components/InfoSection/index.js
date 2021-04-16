cbe5dcac20a63666ea5e71f52ca430576246ee2b
JCPosso <JCPosso@users.noreply.github.com>
2021-04-15 23:43:02 -0500

    creating info section

---
+++ src/Components/InfoSection/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const InfoSection = () => {
+    return (
+        <>
+            <InfoContainer>
+                <InfoWrapper>
+                    <InfoRow>
+                        <Column>
+                        <TextWrapper>
+                            <TopLine>Topline</TopLine>
+                            <Heading>Heading</Heading>
+                            <Subtitle>Subtitle</Subtitle>
+                        </TextWrapper>
+                        </Column>
+                    </InfoRow>
+                </InfoWrapper>
+            </InfoContainer>
+        </>
+    )
+}
+
+export default InfoSection
