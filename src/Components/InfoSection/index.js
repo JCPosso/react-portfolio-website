29ec4bc76ec55591f1e2186b763660ff726c8165
JCPosso <JCPosso@users.noreply.github.com>
2021-04-19 19:01:18 -0500

    styling section part1

--- src/Components/InfoSection/index.js
+++ src/Components/InfoSection/index.js
@@ -1,18 +1,35 @@
 import React from 'react'
-
+import {
+    InfoContainer,
+    InfoWrapper,
+    InfoRow,
+    Column1,
+    Column2,
+    TextWrapper,
+    TopLine,
+    Heading,
+    Subtitle,
+    ImgWrap,
+    Img
+} from './InfoElements';
 const InfoSection = () => {
     return (
         <>
             <InfoContainer>
                 <InfoWrapper>
                     <InfoRow>
-                        <Column>
+                        <Column1>
                         <TextWrapper>
                             <TopLine>Topline</TopLine>
                             <Heading>Heading</Heading>
                             <Subtitle>Subtitle</Subtitle>
                         </TextWrapper>
-                        </Column>
+                        </Column1>
+                        <Column2>
+                            <ImgWrap>
+                            <Img/>
+                            </ImgWrap>
+                        </Column2>
                     </InfoRow>
                 </InfoWrapper>
             </InfoContainer>
